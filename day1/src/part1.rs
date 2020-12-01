00ef5fb45c2137e3e6e793c1194648003b4fd4c7
Clément Sibille <claymeuns@protonmail.com>
2020-12-01 19:46:08 +0100

    Add O(n) solution with sorted array for part1

--- day1/src/part1.rs
+++ day1/src/part1.rs
@@ -6,37 +6,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .collect();
     values.sort();
 
-    for i in 0..values.len() {
-        let first_value = values.get(i).unwrap();
-        let expected_second_value = 2020 - first_value;
-        if can_find_binary_search(&values, expected_second_value) {
-            println!(
-                "{} * {} = {}",
-                first_value,
-                expected_second_value,
-                first_value * expected_second_value
-            );
+    let mut left = 0;
+    let mut right = values.len() - 1;
+    loop {
+        let first_value = values[left];
+        let second_value = values[right];
+
+        let sum = first_value + second_value;
+        if sum == 2020 {
+            println!("{} * {} = {}", first_value, second_value, sum);
             break;
+        } else if sum > 2020 {
+            right -= 1;
+        } else {
+            left += 1
         }
     }
 
     Ok(())
-}
-
-fn can_find_binary_search(values: &Vec<u32>, searched_value: u32) -> bool {
-    let mut start_index = 0;
-    let mut end_index = values.len() - 1;
-    while start_index <= end_index {
-        let current_index = (start_index + end_index) / 2;
-        if let Some(current_value) = values.get(current_index) {
-            if *current_value > searched_value {
-                end_index = current_index - 1;
-            } else if *current_value < searched_value {
-                start_index = current_index + 1;
-            } else {
-                return true;
-            }
-        }
-    }
-    false
 }
