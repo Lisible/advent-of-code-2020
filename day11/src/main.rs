988bdac9c018bce27b9cde6b3f342108eafca50c
Clément Sibille <claymeuns@protonmail.com>
2020-12-11 08:18:51 +0100

    Improve day11-1

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -2,45 +2,49 @@ use std::cmp::{max, min};
 
 const EMPTY_SEAT: u8 = b'L';
 const OCCUPIED_SEAT: u8 = b'#';
-const FLOOR: u8 = b'.';
 
+type SeatMap = Vec<Vec<u8>>;
 fn main() -> Result<(), Error> {
     let input = std::fs::read_to_string("input").map_err(|_| Error::ReadFileError)?;
-    let seat_map: Vec<Vec<u8>> = input
+    let seat_map: SeatMap = input
         .split("\n")
         .map(|s| s.replace("L", "#").bytes().collect())
         .collect();
 
-    let mut previous_seat_map = vec![];
-    let mut seat_map = compute_next_state(&seat_map);
-    while previous_seat_map != seat_map {
-        previous_seat_map = seat_map.clone();
-        seat_map = compute_next_state(&seat_map);
-    }
-
-    let count = seat_map
-        .iter()
-        .flat_map(|v| v)
-        .filter(|&&v| v == OCCUPIED_SEAT)
-        .count();
-    println!("count: {}", count);
+    println!("count: {}", part1(&seat_map));
     Ok(())
 }
 
-fn compute_next_state(seat_map: &Vec<Vec<u8>>) -> Vec<Vec<u8>> {
+fn part1(seat_map: &SeatMap) -> usize {
+    let mut previous_seat_map = vec![];
+    let mut seat_map =
+        compute_next_state(&seat_map, should_become_empty_p1, should_become_occupied_p1);
+    while previous_seat_map != seat_map {
+        previous_seat_map = seat_map.clone();
+        seat_map = compute_next_state(&seat_map, should_become_empty_p1, should_become_occupied_p1);
+    }
+
+    seat_map
+        .iter()
+        .flat_map(|v| v)
+        .filter(|&&v| v == OCCUPIED_SEAT)
+        .count()
+}
+
+fn compute_next_state(
+    seat_map: &SeatMap,
+    empty_rule: fn(usize, usize, &SeatMap) -> bool,
+    occupied_rule: fn(usize, usize, &SeatMap) -> bool,
+) -> SeatMap {
     let width = seat_map[0].len();
     let height = seat_map.len();
 
-    let mut next_state: Vec<Vec<u8>> = seat_map.clone();
+    let mut next_state: SeatMap = seat_map.clone();
     for y in 0..height {
         for x in 0..width {
-            if &next_state[y][x] == &OCCUPIED_SEAT
-                && count_adjacent(x as i32, y as i32, seat_map, OCCUPIED_SEAT) >= 4
-            {
+            if empty_rule(x, y, seat_map) {
                 next_state[y][x] = EMPTY_SEAT
-            } else if &next_state[y][x] == &EMPTY_SEAT
-                && count_adjacent(x as i32, y as i32, seat_map, OCCUPIED_SEAT) == 0
-            {
+            } else if occupied_rule(x, y, seat_map) {
                 next_state[y][x] = OCCUPIED_SEAT
             }
         }
@@ -49,15 +53,20 @@ fn compute_next_state(seat_map: &Vec<Vec<u8>>) -> Vec<Vec<u8>> {
     next_state
 }
 
-fn count_adjacent(x: i32, y: i32, seat_map: &Vec<Vec<u8>>, state: u8) -> usize {
-    let mut number = 0;
-    let min_x = max(x - 1, 0);
-    let min_y = max(y - 1, 0);
-    let max_x = min(x + 1, seat_map[0].len() as i32 - 1);
-    let max_y = min(y + 1, seat_map.len() as i32 - 1);
+fn should_become_empty_p1(x: usize, y: usize, seat_map: &SeatMap) -> bool {
+    &seat_map[y][x] == &OCCUPIED_SEAT
+        && count_adjacent(x as i32, y as i32, seat_map, OCCUPIED_SEAT) >= 4
+}
 
-    for j in min_y..=max_y {
-        for i in min_x..=max_x {
+fn should_become_occupied_p1(x: usize, y: usize, seat_map: &SeatMap) -> bool {
+    &seat_map[y][x] == &EMPTY_SEAT
+        && count_adjacent(x as i32, y as i32, seat_map, OCCUPIED_SEAT) == 0
+}
+
+fn count_adjacent(x: i32, y: i32, seat_map: &SeatMap, state: u8) -> usize {
+    let mut number = 0;
+    for j in max(y - 1, 0)..=min(y + 1, seat_map.len() as i32 - 1) {
+        for i in max(x - 1, 0)..=min(x + 1, seat_map[0].len() as i32 - 1) {
             if j != y || i != x {
                 number += (seat_map[j as usize][i as usize] == state) as usize;
             }
