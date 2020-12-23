00eac7cc93a733604f1c94dee5bf76902c13d516
Clément S <clement.sibille@protonmail.com>
2020-12-23 18:52:45 +0100

    Add day22-2

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,14 +1,16 @@
+use std::collections::hash_map::DefaultHasher;
 use std::collections::VecDeque;
 use std::fmt::{Display, Formatter};
 use std::fs::File;
+use std::hash::{Hash, Hasher};
 use std::io::{BufRead, BufReader};
 use std::str::FromStr;
 
 fn main() -> Result<(), Error> {
-    let file = File::open("input").map_err(|e| Error::InputFileOpenError(e))?;
+    let file = File::open("example_input").map_err(|e| Error::InputFileOpenError(e))?;
     let reader = BufReader::new(file);
     let decks = parse_decks(reader)?;
-    let mut game = Game::with_decks(decks);
+    let mut game = RecursiveCombatGame::with_decks(decks, 1);
     while let GameState::Continue = game.state() {
         game.play_round()?;
     }
@@ -36,48 +38,144 @@ fn parse_decks(reader: impl BufRead) -> Result<Vec<Deck>, Error> {
     })?)
 }
 
-#[derive(Debug)]
-struct Card(u8);
+struct RecursiveCombatGame {
+    players_decks: Vec<Deck>,
+    round: usize,
+    state: GameState,
+    game_number: usize,
+    previous_decks_hashes: Vec<u64>,
+}
 
-impl Display for Card {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
+impl RecursiveCombatGame {
+    pub fn with_decks(players_decks: Vec<Deck>, game_number: usize) -> Self {
+        Self {
+            players_decks,
+            round: 1,
+            state: GameState::Continue,
+            game_number,
+            previous_decks_hashes: vec![],
+        }
+    }
+
+    pub fn play_round(&mut self) -> Result<(), Error> {
+        self.print_round_infos();
+        let mut hasher = DefaultHasher::new();
+        self.players_decks.hash(&mut hasher);
+        let decks_hash = hasher.finish();
+        if self.previous_decks_hashes.contains(&decks_hash) {
+            self.state = GameState::GameOver(0);
+            return Ok(());
+        }
+
+        let mut played_cards = vec![];
+        for player in 0..self.players_decks.len() {
+            let played_card = self.players_decks[player]
+                .0
+                .pop_front()
+                .ok_or(Error::PlayerHasNoCardToPlay)?;
+
+            println!("Player {} plays: {}", player + 1, played_card);
+            played_cards.push((player, played_card));
+        }
+
+        let mut winning_player = 0;
+        if self
+            .players_decks
+            .iter()
+            .enumerate()
+            .all(|(i, d)| d.0.len() >= played_cards[i].1 .0 as usize)
+        {
+            println!("\nPlaying a sub-game to determine the winner...");
+            let mut decks = self.players_decks.clone();
+            for deck in 0..decks.len() {
+                let card_count = played_cards[deck].1 .0;
+                for _ in 0..decks[deck].0.len() - card_count as usize {
+                    decks[deck].0.pop_back();
+                }
+            }
+
+            let mut sub_game = RecursiveCombatGame::with_decks(decks, self.game_number + 1);
+            winning_player = loop {
+                sub_game.play_round();
+                if let GameState::GameOver(winner) = sub_game.state() {
+                    break winner;
+                }
+            };
+            println!("...Anyway back to game {}", self.game_number);
+        } else {
+            played_cards.sort_by_key(|(_, card)| card.0);
+            winning_player = played_cards[played_cards.len() - 1].0;
+        }
+
+        println!(
+            "Player {} wins round {} of game {}!\n",
+            winning_player + 1,
+            self.round,
+            self.game_number
+        );
+        self.players_decks[winning_player]
+            .0
+            .append(&mut played_cards.into_iter().map(|a| a.1).rev().collect());
+
+        self.previous_decks_hashes.push(decks_hash);
+        self.round += 1;
+
+        for (player, player_deck) in self.players_decks.iter().enumerate() {
+            if player_deck.0.is_empty() {
+                self.state = GameState::GameOver((player + 1) % 2)
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn state(&self) -> GameState {
+        self.state
+    }
+
+    pub fn print_round_infos(&self) {
+        println!("-- Round {} (Game {}) --", self.round, self.game_number);
+        for player in 0..self.players_decks.len() {
+            println!(
+                "Player {}'s deck: {}",
+                player + 1,
+                self.players_decks[player]
+            );
+        }
+    }
+
+    pub fn print_post_game_results(&self) {
+        println!("== Post-game results ==");
+        for player in 0..self.players_decks.len() {
+            println!(
+                "Player {}'s deck: {}",
+                player + 1,
+                self.players_decks[player]
+            );
+
+            if !self.players_decks[player].0.is_empty() {
+                println!(
+                    "score: {}",
+                    self.players_decks[player]
+                        .0
+                        .iter()
+                        .rev()
+                        .enumerate()
+                        .fold(0u64, |acc, (i, c)| { acc + (i as u64 + 1) * c.0 as u64 })
+                );
+            }
+        }
     }
 }
 
-impl FromStr for Card {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Card(u8::from_str(s).map_err(|e| Error::ParseCardError(e))?))
-    }
-}
-
 #[derive(Debug)]
-struct Deck(VecDeque<Card>);
-
-impl Display for Deck {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            self.0
-                .iter()
-                .map(|c| c.0.to_string())
-                .collect::<Vec<String>>()
-                .join(",")
-        )
-    }
-}
-
-#[derive(Debug)]
-struct Game {
+struct CombatGame {
     players_decks: Vec<Deck>,
     round: usize,
     state: GameState,
 }
 
-impl Game {
+impl CombatGame {
     pub fn with_decks(players_decks: Vec<Deck>) -> Self {
         Self {
             players_decks,
@@ -87,15 +185,7 @@ impl Game {
     }
 
     pub fn play_round(&mut self) -> Result<(), Error> {
-        println!("-- Round {} --", self.round);
-        for player in 0..self.players_decks.len() {
-            println!(
-                "Player {}'s deck: {}",
-                player + 1,
-                self.players_decks[player]
-            );
-        }
-
+        self.print_round_infos();
         let mut played_cards = vec![];
         for player in 0..self.players_decks.len() {
             let played_card = self.players_decks[player]
@@ -116,8 +206,11 @@ impl Game {
             .append(&mut played_cards.into_iter().map(|a| a.1).rev().collect());
 
         self.round += 1;
-        if self.players_decks.iter().any(|d| d.0.is_empty()) {
-            self.state = GameState::GameOver
+
+        for (player, player_deck) in self.players_decks.iter().enumerate() {
+            if player_deck.0.is_empty() {
+                self.state = GameState::GameOver(player)
+            }
         }
 
         Ok(())
@@ -125,6 +218,17 @@ impl Game {
 
     pub fn state(&self) -> GameState {
         self.state
+    }
+
+    pub fn print_round_infos(&self) {
+        println!("-- Round {} --", self.round);
+        for player in 0..self.players_decks.len() {
+            println!(
+                "Player {}'s deck: {}",
+                player + 1,
+                self.players_decks[player]
+            );
+        }
     }
 
     pub fn print_post_game_results(&self) {
@@ -154,7 +258,41 @@ impl Game {
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum GameState {
     Continue,
-    GameOver,
+    GameOver(usize),
+}
+
+#[derive(Debug, Hash, Clone)]
+struct Card(u8);
+
+impl Display for Card {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl FromStr for Card {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Card(u8::from_str(s).map_err(|e| Error::ParseCardError(e))?))
+    }
+}
+
+#[derive(Debug, Hash, Clone)]
+struct Deck(VecDeque<Card>);
+
+impl Display for Deck {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.0
+                .iter()
+                .map(|c| c.0.to_string())
+                .collect::<Vec<String>>()
+                .join(",")
+        )
+    }
 }
 
 #[derive(Debug)]
