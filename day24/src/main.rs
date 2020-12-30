e0d3f2ab441bef43e5d123b7ee8bd6ea31fae3e8
Clément Sibille <claymeuns@protonmail.com>
2020-12-30 19:54:25 +0100

    Add day24-2

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -19,7 +19,34 @@ fn main() -> Result<(), Error> {
 
     println!("Black tiles: {}", hex_grid.black_tile_count());
 
+    for day in 0..100 {
+        perform_step(&mut hex_grid);
+        println!("Day {}: {}", day + 1, hex_grid.black_tile_count());
+    }
+
     Ok(())
+}
+
+fn perform_step(hex_grid: &mut HexGrid) {
+    let minimum_bounds = hex_grid.minimum_bounds().unwrap();
+    let maximum_bounds = hex_grid.maximum_bounds().unwrap();
+
+    let mut black_tiles = hex_grid.black_tiles.clone();
+    for z in minimum_bounds.2 - 2..maximum_bounds.2 + 2 {
+        for y in minimum_bounds.1 - 2..maximum_bounds.1 + 2 {
+            for x in minimum_bounds.0 - 2..maximum_bounds.0 + 2 {
+                let is_black = hex_grid.is_black((x, y, z));
+                let black_neighbours_count = hex_grid.black_neighbours_count((x, y, z));
+                if is_black && (black_neighbours_count == 0 || black_neighbours_count > 2) {
+                    black_tiles.remove(&(x, y, z));
+                } else if !is_black && black_neighbours_count == 2 {
+                    black_tiles.insert((x, y, z));
+                }
+            }
+        }
+    }
+
+    hex_grid.black_tiles = black_tiles;
 }
 
 fn hex_position(mut line: String) -> HexPosition {
@@ -78,6 +105,54 @@ impl HexGrid {
         }
     }
 
+    pub fn minimum_bounds(&self) -> Option<HexPosition> {
+        self.black_tiles.iter().fold(None, |acc, v| {
+            if acc == None {
+                return Some((v.0, v.1, v.2));
+            }
+
+            let mut current_minimum = acc.unwrap();
+
+            if current_minimum.0 > v.0 {
+                current_minimum.0 = v.0;
+            }
+
+            if current_minimum.1 > v.1 {
+                current_minimum.1 = v.1;
+            }
+
+            if current_minimum.2 > v.2 {
+                current_minimum.2 = v.2;
+            }
+
+            Some(current_minimum)
+        })
+    }
+
+    pub fn maximum_bounds(&self) -> Option<HexPosition> {
+        self.black_tiles.iter().fold(None, |acc, v| {
+            if acc == None {
+                return Some((v.0, v.1, v.2));
+            }
+
+            let mut current_maximum = acc.unwrap();
+
+            if current_maximum.0 < v.0 {
+                current_maximum.0 = v.0;
+            }
+
+            if current_maximum.1 < v.1 {
+                current_maximum.1 = v.1;
+            }
+
+            if current_maximum.2 < v.2 {
+                current_maximum.2 = v.2;
+            }
+
+            Some(current_maximum)
+        })
+    }
+
     pub fn flip_tile(&mut self, position: HexPosition) {
         if self.black_tiles.contains(&position) {
             self.black_tiles.remove(&position);
@@ -88,6 +163,24 @@ impl HexGrid {
 
     pub fn black_tile_count(&self) -> usize {
         self.black_tiles.len()
+    }
+
+    pub fn is_black(&self, position: HexPosition) -> bool {
+        self.black_tiles.contains(&position)
+    }
+
+    pub fn black_neighbours_count(&self, position: HexPosition) -> usize {
+        [
+            (-1, 1, 0),
+            (0, 1, -1),
+            (1, 0, -1),
+            (1, -1, 0),
+            (0, -1, 1),
+            (-1, 0, 1),
+        ]
+        .iter()
+        .filter(|&&p| self.is_black((position.0 + p.0, position.1 + p.1, position.2 + p.2)))
+        .count()
     }
 }
 
